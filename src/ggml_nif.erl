041c9c3fe1fe28241d4009349c3ae1653f78b18c
Ng Ray <mjollnir.ray@gmail.com>
2023-06-14 12:07:14 +0800

    spec

--- src/ggml_nif.erl
+++ src/ggml_nif.erl
@@ -17,6 +17,8 @@
 -export([nbytes/1]).
 -export([set_name/2]).
 
+-export_type([my_context/0, my_tensor/0]).
+
 -nifs([hello/1]).
 -nifs([new_tensor_f32_2d/3]).
 -nifs([new_tensor_f32_1d/2]).
@@ -33,50 +35,67 @@
 -nifs([set_name/2]).
 -nifs([init/1]).
 
+-opaque my_context() :: reference().
+-opaque my_tensor() :: reference().
+
 -on_load(load/0).
 
 load() ->
     erlang:load_nif("priv/ggml_enif", 0).
 
+-spec init(integer()) -> my_context().
 init(_ByteSize) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec new_tensor_f32_2d(my_context(), non_neg_integer(), non_neg_integer()) -> my_tensor().
 new_tensor_f32_2d(_Ctx, _NE0, _NE1) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec new_tensor_f32_1d(my_context(), non_neg_integer()) -> my_tensor().
 new_tensor_f32_1d(_Ctx, _NE) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec tensor_set_f32(my_tensor(), float()) -> my_tensor().
 tensor_set_f32(_T, _V) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec add(my_context(), my_tensor(), my_tensor()) -> my_tensor().
 add(_Ctx, _A, _B) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec mul(my_context(), my_tensor(), my_tensor()) -> my_tensor().
 mul(_Ctx, _A, _B) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec mul_mat(my_context(), my_tensor(), my_tensor()) -> my_tensor().
 mul_mat(_Ctx, _A, _B) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec relu(my_context(), my_tensor()) -> my_tensor().
 relu(_Ctx, _T) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec soft_max(my_context(), my_tensor()) -> my_tensor().
 soft_max(_Ctx, _T) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec graph_compute(my_tensor()) -> my_tensor().
 graph_compute(_F) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec graph_dump_dot(my_tensor(), string()) -> ok.
 graph_dump_dot(_T, _F) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec set_param(my_tensor()) -> ok.
 set_param(_F) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec nbytes(my_tensor()) -> non_neg_integer().
 nbytes(_T) ->
     erlang:nif_error("NIF library not loaded").
 
+-spec set_name(my_tensor(), string()) -> ok.
 set_name(_T, _Name) ->
     erlang:nif_error("NIF library not loaded").
 
